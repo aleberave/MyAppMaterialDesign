f7c2d82c9455783e6b141258f62513cce2bb1748
Aleksandra <lionessa@km.ru>
2022-07-05 23:39:02 +1000

    lesson3 hw +ViewPagerTwoAdapter

--- app/src/main/java/ru/geekbrains/myappmaterialdesign/view/navigation/viewpager/ApiActivity.kt
+++ app/src/main/java/ru/geekbrains/myappmaterialdesign/view/navigation/viewpager/ApiActivity.kt
@@ -2,6 +2,8 @@ package ru.geekbrains.myappmaterialdesign.view.navigation.viewpager
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import ru.geekbrains.myappmaterialdesign.databinding.ActivityApiBinding
 
 class ApiActivity : AppCompatActivity() {
@@ -12,9 +14,29 @@ class ApiActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityApiBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
 
-        binding.tabLayout.setupWithViewPager(binding.viewPager)
+//        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
+//        binding.tabLayout.setupWithViewPager(binding.viewPager)
+
+        binding.viewPager2.adapter = ViewPagerTwoAdapter(this)
+        bindTabLayout()
+    }
+
+    private fun bindTabLayout() {
+        TabLayoutMediator(
+            binding.tabLayout,
+            binding.viewPager2,
+            object : TabLayoutMediator.TabConfigurationStrategy {
+                override fun onConfigureTab(tab: TabLayout.Tab, position: Int) {
+                    tab.text = when (position) {
+                        0 -> "Earth"
+                        1 -> "Mars"
+                        2 -> "System"
+                        else -> "Earth"
+                    }
+                }
+            }
+        ).attach()
     }
 
 }
